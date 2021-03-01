72fa9f3da9bc69c5b752e170ad8f0382c6a7ed99
20197591 <63474422+20197591@users.noreply.github.com>
2021-03-01 06:02:19 -0400

    Add files via upload

---
+++ tarea/tarea 1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace tarea_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            rectangle rectangulo1 = new rectangle();
+
+            rectangulo1.basis = 3;
+            rectangulo1.height = 7;
+
+            
+            rectangle rectangulo2 = new rectangle();
+
+            rectangulo2.basis = 1.5;
+            rectangulo2.height = 4;
+
+
+            rectangle rectangulo3 = new rectangle();
+
+            rectangulo3.basis =7;
+            rectangulo3.height = 10;
+
+            rectangulo1.Grow(10);
+
+
+
+            Console.WriteLine("AREAS");
+
+            Console.WriteLine(rectangulo1.Area());
+            Console.WriteLine(rectangulo2.Area());
+            Console.WriteLine(rectangulo3.Area());
+
+
+            Console.WriteLine("PERIMETROS");
+
+            Console.WriteLine(rectangulo1.perimeter());
+            Console.WriteLine(rectangulo2.perimeter());
+            Console.WriteLine(rectangulo3.perimeter());
+
+
+
+            Console.WriteLine("COMPARACION");
+
+            Console.WriteLine(rectangulo1.Compare(rectangulo2));
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tarea/tarea 1/rectangle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace tarea_1
+{
+    class rectangle
+    {
+        public double basis;
+        public double height;
+
+        public double Area()
+        {
+
+            double area = this.basis * this.height;
+
+            return area;
+
+        }
+
+
+        public double perimeter()
+        {
+
+            double perim;
+
+           perim = this.basis+this.basis+this.height+this.height;
+
+            return perim;
+        }
+
+        public void Grow(double quantity)
+        {
+            this.basis = this.basis + quantity;
+            this.height = this.height + quantity;
+
+
+
+
+        }
+
+
+        public double Compare (rectangle rect)
+        {
+
+            return this.Area() - rect.Area();
+
+
+
+
+        }
+
+    }
+}
